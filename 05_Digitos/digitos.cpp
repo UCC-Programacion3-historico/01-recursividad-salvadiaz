e95ad98b8f79417044f90d263557c4189c5a564f
salvadiaz <salvadiazpesci@gmail.com>
2017-08-23 21:48:09 +0000

    unidad 1 terminada

--- 05_Digitos/digitos.cpp
+++ 05_Digitos/digitos.cpp
@@ -4,14 +4,23 @@
 using namespace std;
 
 unsigned int cociente(unsigned int a, unsigned int b) {
-    return 0;
+    if (b ==0){throw 1;}//error 1: no existe la division por 0
+    if (b<a){return 0;}
+    return 1 + cociente (a-b, b);
 }
 
 
 unsigned int resto(unsigned int a, unsigned int b) {
-    return 0;
+    if (b==0){throw 1;}//error 1: no existe la division por 0
+    if (b<a){return b;}
+    return cociente (a-b, b);
 }
 
 
 void escribir_espaciado(unsigned int num) {
+    if (num == 0){cout<<"";}
+    else {
+        escribir_espaciado (num/10);
+        cout<<num % 10<<"";
+    }
 }
